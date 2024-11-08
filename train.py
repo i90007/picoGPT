173edaf802300594b321be94289d6d56e04e6598
Igor <d.nwton@gmail.com>
2024-11-08 20:52:39 +0200

    minor update

--- train.py
+++ train.py
@@ -8,6 +8,8 @@ from contextlib import nullcontext
 import numpy as np
 
 import torch
+# import sys
+# sys.exit()
 from model import MemorizingGPT
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
@@ -43,7 +45,7 @@ dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported
 compile = True if device == "cuda" else False # use PyTorch 2.0 to compile the model to be faster
 # -----------------------------------------------------------------------------
 class GPTConfig:
-    block_size: int = 256 # (512) how far back does the model look? i.e. context size
+    block_size: int = 128 # (512) how far back does the model look? i.e. context size
     vocab_size: int = 50304 # GPT-2 vocab_size of 50257, padded up to nearest multiple of 64 for efficiency
     n_layer: int = 12 # size of the model
     n_head: int = 12 # size of the model
